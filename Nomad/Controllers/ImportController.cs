e9f3accb0af586e0c1d918928581e6a2256d722c
Piotr Bancarzewski <piotr.bancarzewski3@gmail.com>
2019-04-11 00:00:15 +0200

    added views

---
+++ Nomad/Controllers/ImportController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Core.Services;
+using Nomad.Models.ArticleModels;
+
+namespace Nomad.Controllers
+{
+    public class ImportController : SurfaceController
+    {
+        [HttpGet]
+        public ActionResult ImportArticle(string parentId, int userId)
+        {
+            try
+            {                
+                var path = Server.MapPath("/articles.csv");
+                var listOfObjects = System.IO.File.ReadLines(path).Select(line => new ArticleModel(line)).ToList();
+
+                foreach (var item in listOfObjects)
+                {
+                    IContentService cs = ApplicationContext.Services.ContentService;
+                    Guid parent = Guid.Parse(parentId);
+                    var content = cs.CreateContent(item.Title, parent, "article", userId);
+                    content.SetValue("articleTitle", item.Title);
+                    content.SetValue("articleBody", item.Body);
+                    content.SetValue("articlePublishedDate", item.DatePublished);
+                    //cs.SaveAndPublishWithStatus(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(ex?.Message ?? " inner :" + ex?.InnerException?.Message ?? "");
+            }
+
+            return Content("OK");
+        }
+    }
+}
--- Nomad/Models/ArticleModels/ArticleModel.cs
+++ Nomad/Models/ArticleModels/ArticleModel.cs
@@ -7,6 +7,16 @@ namespace Nomad.Models.ArticleModels
 {
     public class ArticleModel
     {
+        public ArticleModel(string line)
+        {
+            var split = line.Split(',');
+            Title = split[2];
+            Body = split[4];
+            DatePublished = DateTime.UtcNow;
+        }
+        public ArticleModel() { }
+
+
         public string Title { get; set; }
         public string Body { get; set; }
         public string ImagePath { get; set; }
